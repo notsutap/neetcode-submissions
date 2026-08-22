6230dd4e9c73df1c0874d27313e3eebd637829d1
Sutap Pandey <60102751+notsutap@users.noreply.github.com>
2026-08-22 19:50:04 +0530

    Add: shortest-path-in-binary-matrix - submission-0

---
+++ Data Structures & Algorithms/shortest-path-in-binary-matrix/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        if(grid == null || grid[0][0] == 1) return -1;
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
+        Queue<int[]> q = new LinkedList<>();
+        int R = grid.length;
+        int C = grid[0].length;
+        int[][] visited = new int[R][C];
+        q.add(new int[2]);
+        visited[0][0] = 1;
+        int length = 1;
+        while(!q.isEmpty()) {
+            int qLen = q.size();
+            for(int i = 0; i < qLen; i++) {
+                int[] pair = q.poll();
+                int r = pair[0];
+                int c = pair[1];
+                if(r == R - 1 && c == C - 1) return length;
+                for(int[] dir : directions) {
+                    int newR = r + dir[0];
+                    int newC = c + dir[1];
+                    if(newR < 0 || newC < 0 || newR == R || newC == C || visited[newR][newC] == 1 || grid[newR][newC] == 1) continue;
+
+                    q.add(new int[]{newR, newC});
+                    visited[newR][newC] = 1;
+                }
+            }
+            length++;
+        }
+        return -1;
+    }
+}
