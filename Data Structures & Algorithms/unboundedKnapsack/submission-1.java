19aea071da3ff81e80936ba91cd59de294f9a2be
Sutap Pandey <60102751+notsutap@users.noreply.github.com>
2026-08-25 22:40:36 +0530

    Add: unboundedKnapsack - submission-1

---
+++ Data Structures & Algorithms/unboundedKnapsack/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    int[][] cache;
+    public int maximumProfit(List<Integer> profit, List<Integer> weight, int capacity) {
+        cache = new int[profit.size()][capacity + 1];
+        for(int[] row : cache) {
+            Arrays.fill(row, -1);
+        }
+        return dfs(0, profit, weight, capacity);
+    }
+    private int dfs(int i, List<Integer> profit, List<Integer> weight, int capacity) {
+        if(i == profit.size()) {
+            return 0;
+        }
+        if(cache[i][capacity] != -1) {
+            return cache[i][capacity];
+        }
+        cache[i][capacity] = dfs(i + 1, profit, weight, capacity);
+        int newCap = capacity - weight.get(i);
+        if(newCap >= 0) {
+            int p = profit.get(i) + dfs(i, profit, weight, newCap);
+            cache[i][capacity] = Math.max(cache[i][capacity], p);
+        }
+        return cache[i][capacity];
+    }
+}
