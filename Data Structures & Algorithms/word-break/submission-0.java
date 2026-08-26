e17eaf4668c909f62bb2bf5354a01d5cd3f2b867
Sutap Pandey <60102751+notsutap@users.noreply.github.com>
2026-08-26 16:13:55 +0530

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[s.length()] = true;
+        int j = s.length() - 1;
+        for(int i = s.length() - 1; i >= 0; i--) {
+            for(String word : wordDict) {
+                if(i + word.length() <= s.length() && s.startsWith(word, i)) {
+                    if(dp[i + word.length()] == true) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[0];
+    }
+}
